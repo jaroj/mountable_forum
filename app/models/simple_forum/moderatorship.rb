0d15d286a4c3cd3470b667d6bc5933c31e306ec0
Jarosław Jeleniewicz <jarojele@gmail.com>
2013-11-18 10:48:06 +0100

    change validations

--- app/models/simple_forum/moderatorship.rb
+++ app/models/simple_forum/moderatorship.rb
@@ -6,8 +6,8 @@ module SimpleForum
     belongs_to :user,
                :class_name => instance_eval(&SimpleForum.invoke(:user_class)).name
 
-    # validates :forum, :user, :presence => true
-    # validates :user_id, :uniqueness => {:scope => :forum_id, :allow_nil => true}
+    validates :forum_id, :user_id, :presence => true
+    validates :user_id, :uniqueness => {:scope => :forum_id, :allow_nil => true}
 
     #attr_accessible :forum_id, :user_id
 
